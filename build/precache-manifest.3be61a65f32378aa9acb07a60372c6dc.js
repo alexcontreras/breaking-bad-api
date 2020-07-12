4e5d33c6306b8b1bb149ad09624919b316cd60b8
Alex Contreras <ac@jexan.com>
2020-07-11 21:26:24 -0600

    Add compiled project

---
+++ build/precache-manifest.3be61a65f32378aa9acb07a60372c6dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b802bac852627ccb4cbff4a61484bcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2cdd69646348e559ba",
+    "url": "/static/css/main.caf0f2f3.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2cdd69646348e559ba",
+    "url": "/static/js/main.46283fc8.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
